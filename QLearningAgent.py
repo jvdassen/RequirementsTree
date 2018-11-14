9451a7eefe564a67b5f4a5534fe13cadd4a0d260
Jan von der Assen <jan.vda@gmx.ch>
2018-11-14 23:06:39 +0100

    Add a q learning agent

---
+++ QLearningAgent.py
@@ -0,0 +1,46 @@
+from TestEnv import Env
+import random
+import numpy
+
+if __name__ == '__main__':
+    environment = Env()
+
+    alpha = 0.1
+    gamma = 0.6
+    epsilon = 0.1
+
+    number_of_episodes = 1000
+
+    q_table = numpy.zeros([environment.normalizedtree.getNumberOfNodes(), 2])
+
+    for i in range(1, number_of_episodes):
+        state = environment.reset()
+
+        epochs, penalties, reward = 0, 0, 0
+        done = False
+
+        while not done:
+            if random.uniform(0, 1) < epsilon:
+                next_state, reward, done, info  = environment.stepInRandomDirection()
+            else:
+                best_action = numpy.argmax(q_table[state.id])
+                goLeft = best_action == 0
+
+                next_state, reward, done, info = environment.stepInDirection(goLeft)
+
+            action_taken = info
+            old_val = q_table[state.id, action_taken]
+            next_max = numpy.max(q_table[next_state.id])
+
+            new_val = (1 - alpha) * old_val + alpha * (reward + gamma * next_max)
+            q_table[state.id, action_taken] = new_val
+
+            state = next_state
+            epochs += 1
+
+        print('Q table after' + str(i) + ' episodes')
+        print(q_table)
+
+        if i % 100 == 0:
+            print(q_table)
+
